9a3ed388f4397f0b02f59dff3e080f55fca7ade6
franklin <franpad8@gmail.com>
2013-12-19 15:03:44 -0500

    Agregado creacion de topicos y autores

--- demo/apps/clei/urls.py
+++ demo/apps/clei/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns = patterns('demo.apps.clei.views',
         url(r'^registrar/miembrocp/$', 'registrar_miembroCP_view', name="vista_registrar_miembrocp"),
         url(r'^registrar/articulo/$', 'registrar_articulo_view', name="vista_registrar_articulo"),
         url(r'^registrar/evaluacion/$', 'registrar_evaluacion_view', name="vista_registrar_evaluacion"),
+        url(r'^registrar/topico/$', 'registrar_topico_view', name='vista_registrar_topico'),
+        url(r'^registrar/autor/$', 'registrar_autor_view', name='vista_registrar_autor'),
         )
 
 
